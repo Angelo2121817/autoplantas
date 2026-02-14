4b4f5024af584c5dad9822b4aa574ea136708813
Angelo2121817 <metalquimicaconsultoria@gmail.com>
2026-02-14 12:20:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -454,8 +454,8 @@ if "esp_int_m" not in st.session_state:
     st.session_state["esp_int_m"] = 0.12
 if "margem_m" not in st.session_state:
     st.session_state["margem_m"] = 0.50
-    st.session_state["last_canvas_hash"] = None
-
+if "drawing" not in st.session_state:
+    st.session_state["drawing"] = None
 col_left, col_right = st.columns([1, 2], gap="large")
 
 with col_left:
